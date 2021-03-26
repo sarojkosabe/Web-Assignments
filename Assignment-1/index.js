dec9bb93d4cc9157b69559937972ac56f0561d85
sarojkosabe <62433684+sarojkosabe@users.noreply.github.com>
2021-03-26 17:32:44 +0530

    Day 4 Assignment 1 submission

---
+++ Assignment-1/index.js
@@ -0,0 +1,14 @@
+// Using Tag
+var ele = document.getElementsByTagName("h1");
+// ele[0].innerText = "Hey";
+console.log(ele[0].innerText = "Hi");
+
+
+// Using Class 
+var ele2 = document.getElementsByClassName("in");
+console.log(ele2[0].placeholder = "Type anything");
+
+
+// Using id
+var ele3 = document.getElementById("Useid");
+console.log(ele3.innerText = "Bye");
